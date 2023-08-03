119e662aeef45ff82f161459823e59be8f82f4c9
Nickoff <v3136820@gmail.com>
2023-08-03 21:57:12 +0300

    chore: add esLint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'max-lines-per-function': ['error', 40],
+  },
+}
