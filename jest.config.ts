9226b900fd1bc3ae11417fd5d92a30f2ecb82036
SpacePocket <space1985pocket@hmail.com>
2023-08-09 17:27:06 +0300

    refactor: update sass structure

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,11 @@ const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   moduleDirectories: ['node_modules', '<rootDir>'],
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  passWithNoTests: true,
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!(tsx-vanilla)/)'],
+  transform: {
+    '^.+\\.(ts|js)x?$': 'ts-jest',
+  },
 };
 
 export default config;
