40dbc214dd9294b186b9053a4306bcf717244cb4
SpacePocket <space1985pocket@hmail.com>
2023-08-09 17:19:11 +0300

    feat: add sass structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,1 @@
-import '@components/hello-world.scss';
-import { heading } from '@components/hello-world';
-import Img from '@assets/images/rsschool.png';
-
-const img = new Image();
-img.src = Img;
-
-document.body.append(heading, img);
+import './styles/main.scss';
