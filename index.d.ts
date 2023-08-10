ee605604bf28ae1523ab5d30a1b8661c4bc06451
Marat Bondarenko <nowaylifer@gmail.com>
2023-08-10 20:01:39 +0300

    feat: add better svg import and some component enhancements

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,4 @@ declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.png';
 declare module '*.gif';
-
-declare module '*.svg' {
-  const value: string;
-  export default value;
-}
+declare module '*.svg';
