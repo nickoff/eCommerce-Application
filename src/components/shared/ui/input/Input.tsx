a8a58b59c517323cf7b79ba28b05416891a2a683
Nickoff <v3136820@gmail.com>
2023-08-11 23:25:24 +0300

    feat: add style Input

--- src/components/shared/ui/input/Input.tsx
+++ src/components/shared/ui/input/Input.tsx
@@ -7,8 +7,9 @@ import { ValidateType } from '@shared/validation';
 import { passwordSchema } from '@shared/validation/validationSchemas';
 
 enum Styles {
-  input = 'input',
-  input_invalid = 'input_invalid',
+  INPUT = 'input',
+  INPUT_INVALID = 'input_invalid',
+  INPUT__LABEL = 'input__label',
 }
 
 export enum InputTypes {
@@ -37,44 +38,48 @@ export class Input extends Component<IInputProps> {
 
   private handleBlur = (event: Event): void => {
     if (!event.target || !(event.target instanceof HTMLInputElement)) return;
+
     try {
       this.validSchema.validateSync({ input: event.target.value });
-      this.props.isError = false;
+      this.setProps({ isError: false });
     } catch (error) {
       if (error instanceof yup.ValidationError) {
         this.errorMessage = error.message;
       }
 
-      this.props.isError = true;
+      this.setProps({ isError: true });
     }
   };
 
   private handleInput = (event: Event): void => {
     if (!event.target || !(event.target instanceof HTMLInputElement)) return;
     this.inputValue = event.target.value;
-    this.getContent();
   };
 
   private handleFocus = (event: Event): void => {
     if (!event.target || !(event.target instanceof HTMLInputElement)) return;
     if (this.props.isError) {
-      event.target.nextSibling?.remove();
+      event.target.previousSibling?.childNodes[1]?.remove();
+      event.target.parentElement?.classList.remove(Styles.INPUT_INVALID);
     }
   };
 
   render(): JSX.Element {
     return (
-      <div className={Styles.input}>
-        <p>{this.props.labelText}</p>
+      <div className={Styles.INPUT + (this.props.isError ? ` ${Styles.INPUT_INVALID}` : '')}>
+        <div className={Styles.INPUT__LABEL}>
+          <p>{this.props.labelText}</p>
+          {this.props.isError && <p>{this.errorMessage}</p>}
+        </div>
+
         <input
-          className={Styles.input + (this.props.isError ? ` ${Styles.input_invalid}` : '')}
+          className={this.props.isError ? ` ${Styles.INPUT_INVALID}` : ''}
           type={this.props.type}
           onblur={this.handleBlur}
           oninput={this.handleInput}
           onfocus={this.handleFocus}
           value={this.inputValue}
         />
-        {this.props.isError && <p style={{ color: 'red' }}>{this.errorMessage}</p>}
       </div>
     );
   }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import './styles/main.scss';
 import { Input, InputTypes } from '@components/shared/ui/input/Input';
 
 const newInput = new Input({
