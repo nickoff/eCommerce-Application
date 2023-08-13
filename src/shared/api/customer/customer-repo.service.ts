4fbede74e06f9780c11ecf74517f05158645eee8
Marat Bondarenko <nowaylifer@gmail.com>
2023-08-13 16:23:57 +0300

    feat: creat service to create & get customer from commertools api

---
+++ src/shared/api/customer/customer-repo.service.ts
@@ -0,0 +1,92 @@
+import { BaseAddress, type Customer, type CustomerDraft } from '@commercetools/platform-sdk';
+import { type HttpErrorType } from '@commercetools/sdk-client-v2';
+import { isHttpErrorType } from '@shared/utils/type-guards';
+
+import { INewCustomer, ICustomerCredentials } from '@shared/interfaces/customer.interface';
+import apiRoot from '../api-root';
+
+class CustomerRepoService {
+  static async createCustomer(customerDraft: CustomerDraft): Promise<Customer | HttpErrorType> {
+    try {
+      const response = await apiRoot
+        .customers()
+        .post({
+          body: customerDraft,
+        })
+        .execute();
+
+      const { customer } = response.body;
+      return customer;
+    } catch (error) {
+      if (isHttpErrorType(error)) {
+        return error;
+      }
+
+      throw error;
+    }
+  }
+
+  static async getCustomerByCredentials({ email, password }: ICustomerCredentials): Promise<Customer | HttpErrorType> {
+    try {
+      const response = await apiRoot
+        .me()
+        .login()
+        .post({
+          body: {
+            email,
+            password,
+          },
+        })
+        .execute();
+
+      const { customer } = response.body;
+      return customer;
+    } catch (error) {
+      if (isHttpErrorType(error)) {
+        return error;
+      }
+
+      throw error;
+    }
+  }
+
+  static async getCustomerById(ID: string): Promise<Customer | HttpErrorType> {
+    try {
+      const response = await apiRoot.customers().withId({ ID }).get().execute();
+      const customer = response.body;
+
+      return customer;
+    } catch (error) {
+      if (isHttpErrorType(error)) {
+        return error;
+      }
+
+      throw error;
+    }
+  }
+
+  static createCustomerDraft(customerData: INewCustomer): CustomerDraft {
+    const { firstName, lastName, email, password, dateOfBirth, shippingAddress, billingAddress } = customerData;
+
+    const addresses: BaseAddress[] = [shippingAddress];
+
+    if (billingAddress) {
+      addresses.push(billingAddress);
+    }
+
+    const customerDraft: CustomerDraft = {
+      firstName,
+      lastName,
+      email,
+      password,
+      dateOfBirth,
+      addresses,
+      defaultShippingAddress: 0,
+      defaultBillingAddress: billingAddress ? 1 : undefined,
+    };
+
+    return customerDraft;
+  }
+}
+
+export default CustomerRepoService;
