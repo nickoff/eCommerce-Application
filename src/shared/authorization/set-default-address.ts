147bfffe2397b5d788301e066815b3db07bca4ef
Space Pocket <space1985pocket@gmail.com>
2023-08-15 08:29:14 +0300

    update func set-default-address

--- src/shared/authorization/set-default-address.ts
+++ src/shared/authorization/set-default-address.ts
@@ -1,7 +1,9 @@
+import { MouseEvtName } from '@shared/constants/events';
+
 const setDefaultAdress = (): void => {
   const billingCheckbox: HTMLInputElement = <HTMLInputElement>document.querySelector('.billing-checkbox');
 
-  billingCheckbox.addEventListener('change', () => {
+  billingCheckbox.addEventListener(MouseEvtName.CHANGE, () => {
     const shipingWrapper: HTMLElement = <HTMLElement>document.querySelector('.address-shiping');
     const billingWrapper: HTMLElement = <HTMLElement>document.querySelector('.billing-wrapper');
     const shipingInputs = shipingWrapper.querySelectorAll('input');
@@ -9,8 +11,8 @@ const setDefaultAdress = (): void => {
 
     if (billingCheckbox.checked) {
       shipingInputs.forEach((input, index) => {
-        input.addEventListener('change', () => {
-          bilingInputs[index].value = input.value;
+        input.addEventListener(MouseEvtName.CHANGE, () => {
+          if (billingCheckbox.checked) bilingInputs[index].value = input.value;
         });
         bilingInputs[index].value = input.value;
         bilingInputs[index].disabled = true;
