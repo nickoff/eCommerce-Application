75a93a0045484780f0983f8e85fb36d38148f342
Nickoff <v3136820@gmail.com>
2023-08-15 09:19:27 +0300

    chore: remove test view from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,4 @@
-import { InputName } from '@shared/enums';
 import './styles/main.scss';
 import Header from '@components/layout/header/header';
-import { Input } from '@components/shared/ui/input/Input';
 
-const myInput = new Input({
-  name: InputName.Password,
-  labelText: 'Email',
-});
-
-document.body.append(new Header().render(), myInput.render());
+document.body.append(new Header().render());
