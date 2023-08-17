a1a06bca52b890b5315318c6fb82d5556fdb50e1
Nickoff <v3136820@gmail.com>
2023-08-17 21:39:13 +0300

    feat: implement object data to formDataBuilder

--- src/shared/utils/formData-builder.ts
+++ src/shared/utils/formData-builder.ts
@@ -1,9 +1,10 @@
 import { Input } from '@components/shared/ui/input/input';
 import Select from '@components/shared/ui/select/select';
+import { IAddress, INewCustomer } from '@shared/interfaces/customer.interface';
 
 type ObjectData = Record<string, string | number | boolean | Date | string[]>;
 
-export function formDataBuilder(form: HTMLFormElement): ObjectData {
+function formRegValidator(form: HTMLFormElement): boolean {
   let hasErrors = false;
   const arrInput = Array.from(form.elements).filter(
     (el) => el instanceof HTMLInputElement || el instanceof HTMLSelectElement,
@@ -24,21 +25,59 @@ export function formDataBuilder(form: HTMLFormElement): ObjectData {
       }
     }
   });
-  const data: ObjectData = {};
-  if (!hasErrors) {
+  // eslint-disable-next-line no-console
+  console.log(hasErrors);
+  return hasErrors;
+}
+
+function getShippingAddress(data: ObjectData): IAddress {
+  return {
+    country: Array.isArray(data.country) ? data.country[0] : (data.country as string),
+    city: Array.isArray(data.city) ? data.city[0] : (data.city as string),
+    street: Array.isArray(data.streetName) ? data.streetName[0] : (data.streetName as string),
+    postalCode: Array.isArray(data.postalCode) ? data.postalCode[0] : (data.postalCode as string),
+  };
+}
+
+function getBillingAddress(data: ObjectData): IAddress {
+  return {
+    country: Array.isArray(data.country) ? data.country[1] : (data.country as string),
+    city: Array.isArray(data.city) ? data.city[1] : (data.city as string),
+    street: Array.isArray(data.streetName) ? data.streetName[1] : (data.streetName as string),
+    postalCode: Array.isArray(data.postalCode) ? data.postalCode[1] : (data.postalCode as string),
+  };
+}
+
+export function formDataBuilder(form: HTMLFormElement): INewCustomer {
+  const dataDraft: ObjectData = {};
+  if (!formRegValidator(form)) {
+    const arrInput = Array.from(form.elements).filter(
+      (el) => el instanceof HTMLInputElement || el instanceof HTMLSelectElement,
+    );
+
     arrInput.forEach((value) => {
       const inputValue = value as HTMLInputElement;
-      if (data[inputValue.name]) {
-        if (Array.isArray(data[inputValue.name])) {
-          (data[inputValue.name] as string[]).push(inputValue.value);
+      if (dataDraft[inputValue.name]) {
+        if (Array.isArray(dataDraft[inputValue.name])) {
+          (dataDraft[inputValue.name] as string[]).push(inputValue.value);
         } else {
-          data[inputValue.name] = [data[inputValue.name] as string, inputValue.value];
+          dataDraft[inputValue.name] = [dataDraft[inputValue.name] as string, inputValue.value];
         }
       } else {
-        data[inputValue.name] = inputValue.value;
+        dataDraft[inputValue.name] = inputValue.value;
       }
     });
   }
+
+  const data: INewCustomer = {
+    shippingAddress: getShippingAddress(dataDraft),
+    billingAddress: dataDraft.useShippingAddress === 'on' ? undefined : getBillingAddress(dataDraft),
+    firstName: dataDraft.firstName as string,
+    lastName: dataDraft.lastName as string,
+    email: dataDraft.email as string,
+    password: Array.isArray(dataDraft.password) ? dataDraft.password[0] : (dataDraft.password as string),
+  };
+
   // eslint-disable-next-line no-console
   console.log(data);
   return data;
