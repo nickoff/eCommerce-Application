2790e948038b285b4c99c16edfad1ecef4e78278
Nickoff <v3136820@gmail.com>
2023-08-17 13:38:00 +0300

    feat: add validation field to formDataBuilder

--- src/shared/utils/formData-builder.ts
+++ src/shared/utils/formData-builder.ts
@@ -1,6 +1,9 @@
+import { Input } from '@components/shared/ui/input/input';
+
 export function formDataBuilder(form: HTMLFormElement): void {
   const arrInput = Array.from(form.elements).filter((el) => el instanceof HTMLInputElement);
-
-  // eslint-disable-next-line no-console
-  console.log(arrInput);
+  arrInput.forEach((el) => {
+    if (el instanceof HTMLInputElement && el.type === 'checkbox') return;
+    (el.parentElement?.getComponent() as Input).validation();
+  });
 }
