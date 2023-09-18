b2e51eb2883cf71601309aa29e561bc01ae9df94
Nickoff <v3136820@gmail.com>
2023-09-19 00:51:42 +0300

    feat: add modify product quantity to Cart

--- src/components/entities/line-item/line-item-card/line-item-card.tsx
+++ src/components/entities/line-item/line-item-card/line-item-card.tsx
@@ -1,7 +1,7 @@
 import { element } from 'tsx-vanilla';
 import store from '@app/store/store';
 import cx from 'clsx';
-import { Component } from '@shared/lib';
+import { Component, Child } from '@shared/lib';
 import TrashIcon from '@assets/icons/trash.element.svg';
 import CartRepoService from '@shared/api/cart/cart-repo.service';
 import { isHttpErrorType } from '@shared/utils/type-guards';
@@ -17,8 +17,10 @@ class LineItemCard extends Component<ILineItemProps> {
 
   private cartId = store.getState().cart?.id;
 
+  @Child('output', true) quantityOutput!: HTMLOutputElement;
+
   render(): JSX.Element {
-    const { id, name, price, discountedPrice, totalPrice, quantity, image } = this.props.lineItemData;
+    const { id, name, productId, price, discountedPrice, totalPrice, quantity, image } = this.props.lineItemData;
 
     return (
       <div className={s.lineItemCard}>
@@ -29,12 +31,23 @@ class LineItemCard extends Component<ILineItemProps> {
           <p className={s.prodName}>{name}</p>
         </div>
         <div className={s.lineItemCardRightSide}>
-          <p className={s.prodPricePerUnit}>{`$ ${discountedPrice ? discountedPrice / 100 : price / 100}`}</p>
-          <p className={s.prodQuantity}>{`x${quantity}`}</p>
-          <p className={s.prodTotalItem}>{`$ ${totalPrice / 100}`}</p>
-          <button className={cx(btn, btnOutLine, s.removeToCartBtn)} onclick={this.onRemoveToCartClick.bind(this, id)}>
-            {TrashIcon.cloneNode(true)}
-          </button>
+          <p className={s.prodPricePerUnit}>
+            {`$ ${discountedPrice ? discountedPrice / 100 : price / 100}`}
+            <div className={s.quantityCounter}>
+              <button onclick={this.decreaseQuantity.bind(this, id)}>-</button>
+              <output>{`x${quantity}`}</output>
+              <button onclick={this.increaseQuantity.bind(this, productId)}>+</button>
+            </div>
+          </p>
+          <p className={s.prodTotalItem}>
+            {`$ ${totalPrice / 100}`}
+            <button
+              className={cx(btn, btnOutLine, s.removeToCartBtn)}
+              onclick={this.onRemoveToCartClick.bind(this, id)}
+            >
+              {TrashIcon.cloneNode(true)}
+            </button>
+          </p>
         </div>
       </div>
     );
@@ -46,6 +59,33 @@ class LineItemCard extends Component<ILineItemProps> {
 
     if (!versionCart || !this.cartId || !lineItemId) return;
     const updateCart = await CartRepoService.removeLineItemToCart(store.apiRoot, lineItemId, versionCart, this.cartId);
+    if (!isHttpErrorType(updateCart)) {
+      store.setState({ cart: updateCart });
+    }
+  }
+
+  private async decreaseQuantity(id: string): Promise<void> {
+    const versionCart = store.getState().cart?.version;
+    const lineItemId = store.getState().cart?.lineItems.find((lineItem) => lineItem.id === id)?.id;
+
+    if (!versionCart || !this.cartId || !lineItemId) return;
+    const updateCart = await CartRepoService.removeLineItemToCart(
+      store.apiRoot,
+      lineItemId,
+      versionCart,
+      this.cartId,
+      1,
+    );
+    if (!isHttpErrorType(updateCart)) {
+      store.setState({ cart: updateCart });
+    }
+  }
+
+  private async increaseQuantity(poductId: string): Promise<void> {
+    const versionCart = store.getState().cart?.version;
+
+    if (!versionCart || !this.cartId) return;
+    const updateCart = await CartRepoService.addLineItemToCart(store.apiRoot, poductId, versionCart, this.cartId);
     if (!isHttpErrorType(updateCart)) {
       store.setState({ cart: updateCart });
     }
