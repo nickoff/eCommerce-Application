60b36d82ba9e9edb636dd1e36b6de9e289ac4953
Nickoff <v3136820@gmail.com>
2023-09-18 14:04:33 +0300

    feat: display empty Cart message and link  catalog

--- src/pages/basket/basket.tsx
+++ src/pages/basket/basket.tsx
@@ -7,19 +7,47 @@ import { isHttpErrorType } from '@shared/utils/type-guards';
 import { btn } from '@styles/shared/index.module.scss';
 import CartRepoService from '@shared/api/cart/cart-repo.service';
 import * as s from './basket.module.scss';
-import { BasketPageText } from './config';
+import { BasketPageText, linksConfig } from './config';
 
 @PageTitle('Basket')
 class BasketPage extends Component {
+  constructor(...args: IProps[]) {
+    super(...args);
+    store.subscribe('cart', this);
+  }
+
   private cartId = store.getState().cart?.id;
 
   render(): JSX.Element {
+    const { cart } = store.getState();
     return (
-      <div className={s.basketPageHeader}>
-        <h3>{BasketPageText.Title}</h3>
-        <button className={cx(btn)} onclick={this.onRemoveAllCartClick.bind(this)}>
-          {BasketPageText.BntRemoveAll}
-        </button>
+      <div className={s.basketPageWrapper}>
+        <div className={s.basketPageHeader}>
+          <h3>{BasketPageText.Title}</h3>
+          {cart && cart?.lineItems.length > 0 && (
+            <button className={cx(btn)} onclick={this.onRemoveAllCartClick.bind(this)}>
+              {BasketPageText.BntRemoveAll}
+            </button>
+          )}
+        </div>
+        {cart && cart?.lineItems.length ? <div>1</div> : this.emptyCart()}
+      </div>
+    );
+  }
+
+  private emptyCart(): JSX.Element {
+    return (
+      <div className={s.basketEmpty}>
+        <p className={cx(s.basketEmptyArticle)}>{BasketPageText.EmptyCart}</p>
+        <ul className={cx(s.basketEmptyNavList)}>
+          {linksConfig.map((link) => (
+            <li className={cx(s.basketEmptyNavItem)}>
+              <a className={cx(s.basketEmptyNavLink)} href={link.route} dataset={{ navigo: '' }}>
+                {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
